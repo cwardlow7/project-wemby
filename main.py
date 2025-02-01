4826ecd0fc7019ec473edd4c4369e9d274366a09
cwardlow7 <cwardlow7@gmail.com>
2025-02-01 16:55:34 -0500

    First questions

---
+++ main.py
@@ -0,0 +1,16 @@
+#Goal Is To Find Most Similar Player to Victor Wembenyama
+
+# Python 3.11
+# Imports
+
+# 1. Pull in data
+    # Which source has the best data?
+    # Are we going to need to scrape data off the web or can we use an API?
+    # Stats and Physical Attributes need to be taken into account
+    # European stats? International? 
+    # Looking on a season by season basis?  Not a lot of career stats to pull from so far
+
+# 2. What methods will we be using?  
+    # Clustering? This makes most sense on surface but is there a way to take a more supervised approach?
+    # Can we confirm our findings by using obvious comparisons?  i.e. Kobe and Jordan should be near each other 
+    # Anything else we should consider?
